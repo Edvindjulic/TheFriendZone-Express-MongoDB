7fed8e2a12f4180063a7b305c8b8353c00f26417
lysmac <calle@lysmask.se>
2023-04-18 13:52:17 +0200

    connection to atlas database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,16 @@
-// import { app } from './app';
+import mongoose from "mongoose";
+import { app } from "./app";
 
 // HÄR SKRIVER NI KODEN FÖR ATT ANSLUTA TILL DATABASEN OCH STARTA SERVERN!
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://superuser:DiiWV2H7irGTztEK@clusterfudge.wfhk6bq.mongodb.net/?retryWrites=true&w=majority"
+  );
+  console.log("Connected to database");
+
+  app.listen(3000, () => {
+    console.log("Server is running :)");
+  });
+}
+
+main().catch(console.error);
