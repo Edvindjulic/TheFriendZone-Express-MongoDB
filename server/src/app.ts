cb56409aa2ae5a2ae55ccd796b75108c70f4d45e
lysmac <calle@lysmask.se>
2023-04-19 14:32:28 +0200

    moved to a controller file and installed cookie-session

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import cookieSession from "cookie-session";
 import express from "express";
 import { MongoClient } from "mongodb";
 import userRouter from "./users/user-router";
@@ -6,6 +7,15 @@ export const app = express();
 
 // Global Middlewares
 app.use(express.json());
+app.use(
+  cookieSession({
+    name: "login",
+    secure: false,
+    httpOnly: true,
+    secret: "as98d7asyudbahs8d97a6digas78d866u",
+    maxAge: 1000 * 20,
+  })
+);
 // TODO: Insert cookie-stuff
 
 const uri =
--- server/src/users/user-router.ts
+++ server/src/users/user-router.ts
@@ -1,63 +1,11 @@
-import argon2 from "argon2";
 import express from "express";
-import { db } from "../app";
+
+import { getAllUsers, registerUser } from "./user-controller";
 
 const userRouter = express
   .Router()
-  // Get all users (admin)
-  .get("/users", async (req, res) => {
-    try {
-      const userCollection = db.collection("users");
-
-      const users = await userCollection.find().toArray();
-
-      res.status(200).json({ message: "All users", data: users });
-    } catch (error) {
-      console.error("Error finding users:", error);
-      res.status(500).json({
-        message: "Error finding users",
-        error: (error as any).message,
-      });
-    }
-  })
-  // Register a new user
-  .post("/users/register", async (req, res) => {
-    try {
-      const { username, email, password } = req.body;
-      const hashedPassword = await argon2.hash(password);
-      const userCollection = db.collection("users");
-      const user = { username, email, password: hashedPassword };
-
-      const allUsers = await userCollection.find().toArray();
-
-      const mailCheck = allUsers.find((user) => user.email === email);
-      const usernameCheck = allUsers.find((user) => user.username === username);
-      if (mailCheck) {
-        return res.status(400).json({
-          message: "Email already in use",
-          error: "Email already in use",
-        });
-      }
-      if (usernameCheck) {
-        return res.status(400).json({
-          message: "Username already in use",
-          error: "Username already in use",
-        });
-      }
-
-      const result = await userCollection.insertOne(user);
-      res.status(201).json({
-        message: "User inserted",
-        data: { ...user, _id: result.insertedId },
-      });
-    } catch (error) {
-      console.error("Error inserting user:", error);
-      res.status(500).json({
-        message: "Error inserting user",
-        error: (error as any).message,
-      });
-    }
-  })
+  .get("/users", getAllUsers)
+  .post("/users/register", registerUser)
   .put("/users", async (req, res) => {})
   .delete("/users", async (req, res) => {});
 
