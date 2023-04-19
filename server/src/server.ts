ecc0fb0cd518217fe86d8a7bdff496518376b64b
lysmac <calle@lysmask.se>
2023-04-19 09:43:30 +0200

    hej

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,14 @@
+import express from "express";
 import mongoose from "mongoose";
 import { app } from "./app";
 
+app.use(express.json());
+
 // HÄR SKRIVER NI KODEN FÖR ATT ANSLUTA TILL DATABASEN OCH STARTA SERVERN!
 async function main() {
+  mongoose.set("strictQuery", true);
   await mongoose.connect(
-    "mongodb+srv://superuser:DiiWV2H7irGTztEK@clusterfudge.wfhk6bq.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://superuser:DiiWV2H7irGTztEK@clusterfudge.wfhk6bq.mongodb.net/thefriendzone?retryWrites=true&w=majority"
   );
   console.log("Connected to database");
 
