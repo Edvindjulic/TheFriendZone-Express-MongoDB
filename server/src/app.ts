5539d402a7fc8e6c2627a99481a118ccda4d43a7
lysmac <calle@lysmask.se>
2023-04-19 11:25:26 +0200

    created user router and it works

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,42 +1,23 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import userRouter from "./users/user-router";
 
 export const app = express();
 
+// Global Middlewares
 app.use(express.json());
+// TODO: Insert cookie-stuff
 
 const uri =
   "mongodb+srv://superuser:DiiWV2H7irGTztEK@clusterfudge.wfhk6bq.mongodb.net/thefriendzone?retryWrites=true&w=majority";
 export const client = new MongoClient(uri);
 const dbName = "coolmegadatabase";
-const db = client.db(dbName);
+export const db = client.db(dbName);
 
-// async function testFunction() {
-//   console.log("hejsan");
-//   const userCollection = db.collection("horses");
-//   userCollection.collectionName;
+// app.use(postRouter);
+app.use(userRouter);
 
-//   userCollection.insertOne({ name: "Simon", complexity: 1 });
-// }
+// const postRouter
 
-app.post("/horses", async (req, res) => {
-  try {
-    const { name, complexity } = req.body;
-    const userCollection = db.collection("horses");
-
-    const result = await userCollection.insertOne({ name, complexity });
-    const insertedDocument = { _id: result.insertedId, name, complexity }; // Create the inserted document object
-
-    res.status(201).json({ message: "Horse inserted", data: insertedDocument });
-  } catch (error) {
-    console.error("Error inserting horse:", error);
-    res.status(500).json({
-      message: "Error inserting horse",
-      error: (error as any).message,
-    });
-  }
-});
-
-// testFunction().catch(console.error);
 // SKRIV DIN SERVERKOD HÄR!
 // app.get("/users");
