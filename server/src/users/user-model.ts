f6b265c1bab136d92632daae84c83c59bee643ab
lysmac <calle@lysmask.se>
2023-04-20 11:12:11 +0200

    user model created and exported properly from index.ts

--- server/src/users/user-model.ts
+++ server/src/users/user-model.ts
@@ -1,3 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export type User = mongoose.InferSchemaType<typeof userSchema>;
+
+export const UserModel = mongoose.model("User", userSchema);
+
 // username
 // password
 // isAdmin: { type: Boolean, default: false }
