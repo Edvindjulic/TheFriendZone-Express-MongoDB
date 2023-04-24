a7b4043aa3ff95b2d12b2d4037d03e3ce61a6d75
edvindjulic <djulicedvin@gmail.com>
2023-04-24 10:17:57 +0200

    Remove email from SignUp form and make SignIn form look the same (syntax)

--- client/src/components/SignIn.tsx
+++ client/src/components/SignIn.tsx
@@ -1,103 +1,84 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Checkbox from "@mui/material/Checkbox";
-import CssBaseline from "@mui/material/CssBaseline";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import * as React from "react";
-import logo from "../assets/logo.png";
-import Posts from "./PostComponent";
+import { Box, Button, Paper, TextField } from "@mui/material";
+import { useFormik } from "formik";
 
-export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
+interface SigninValues {
+  username: string;
+  password: string;
+}
+
+export default function SignInForm() {
+  const formik = useFormik<SigninValues>({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values: SigninValues) => {
+      console.log(values);
+      // VAD GÖR VI HÄR BACKEND?
+    },
+  });
 
   return (
-    <Grid container component="main" sx={{ height: "100vh" }}>
-      <CssBaseline />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        {/* Logga in form */}
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
+    <Paper
+      elevation={6}
+      sx={{
+        width: "40%",
+        margin: "auto",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": {
+            m: 1,
+            width: "20rem",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Kom ihåg mig"
-            />
-            <Grid container sx={{ textAlign: "center" }}>
-              <Grid item xs={12}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{
-                    mt: 3,
-                    mb: 2,
-                    bgcolor: "#E3653D",
-                    "&:hover": {
-                      bgcolor: "#488BA5",
-                    },
-                  }}
-                >
-                  Logga in
-                </Button>
-              </Grid>
-              <Grid item xs={12}>
-                <Link href="signup" variant="body2">
-                  {"Har du inget konto? Registrera dig här"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Grid>
-      <Grid item xs={12} sm={4} md={7}>
-        {/* Inläggs-kortet */}
-        <Box sx={{ mx: 4, my: 2 }}>
-          <Posts />
-        </Box>
-      </Grid>
-    </Grid>
+            gap: "0.5rem",
+            margin: "2rem",
+            height: "auto",
+          },
+        }}
+        noValidate
+        onSubmit={formik.handleSubmit}
+      >
+        <TextField
+          id="username"
+          name="username"
+          label="Användarnamn"
+          type="text"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.username && Boolean(formik.errors.username)
+          }
+          helperText={
+            formik.touched.username && formik.errors.username
+          }
+        />
+        <TextField
+          id="password"
+          name="password"
+          label="Lösenord"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.password && Boolean(formik.errors.password)
+          }
+          helperText={
+            formik.touched.password && formik.errors.password
+          }
+        />
+        <Button color="secondary" type="submit" variant="contained">
+          Logga in
+        </Button>
+      </Box>
+    </Paper>
   );
 }
--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,46 +1,26 @@
-import {
-  Box,
-  Button,
-  Paper,
-  TextField,
-} from "@mui/material";
+import { Box, Button, Paper, TextField } from "@mui/material";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
 interface SignupValues {
-  email: string;
   username: string;
   password: string;
   confirmPassword: string;
 }
 
 const SignupSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Ange en giltig e-postadress")
-    .required("E-postadress är obligatoriskt"),
-  username: Yup.string().required(
-    "Användarnamn är obligatoriskt"
-  ),
+  username: Yup.string().required("Användarnamn är obligatoriskt"),
   password: Yup.string()
     .required("Lösenord är obligatoriskt")
-    .min(
-      6,
-      "Lösenordet måste innehålla minst 6 tecken"
-    ),
+    .min(6, "Lösenordet måste innehålla minst 6 tecken"),
   confirmPassword: Yup.string()
-    .required(
-      "Bekräfta lösenord är obligatoriskt"
-    )
-    .oneOf(
-      [Yup.ref("password")],
-      "Lösenorden matchar inte"
-    ),
+    .required("Bekräfta lösenord är obligatoriskt")
+    .oneOf([Yup.ref("password")], "Lösenorden matchar inte"),
 });
 
 export default function SignUpForm() {
   const formik = useFormik<SignupValues>({
     initialValues: {
-      email: "",
       username: "",
       password: "",
       confirmPassword: "",
@@ -72,90 +52,63 @@ export default function SignUpForm() {
             display: "flex",
             flexDirection: "column",
             gap: "0.5rem",
-            margin: '2rem',
-            height: 'auto'
+            margin: "2rem",
+            height: "auto",
           },
         }}
         noValidate
         onSubmit={formik.handleSubmit}
       >
-          <TextField
-            id="email"
-            name="email"
-            label="E-postadress"
-            type="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={
-              formik.touched.email &&
-              Boolean(formik.errors.email)
-            }
-            helperText={
-              formik.touched.email &&
-              formik.errors.email
-            }
-          />
-          <TextField
-            id="username"
-            name="username"
-            label="Användarnamn"
-            type="text"
-            value={formik.values.username}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={
-              formik.touched.username &&
-              Boolean(formik.errors.username)
-            }
-            helperText={
-              formik.touched.username &&
-              formik.errors.username
-            }
-          />
-          <TextField
-            id="password"
-            name="password"
-            label="Lösenord"
-            type="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={
-              formik.touched.password &&
-              Boolean(formik.errors.password)
-            }
-            helperText={
-              formik.touched.password &&
-              formik.errors.password
-            }
-          />
-          <TextField
-            id="confirmPassword"
-            name="confirmPassword"
-            label="Bekräfta lösenord"
-            type="password"
-            value={formik.values.confirmPassword}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={
-              formik.touched.confirmPassword &&
-              Boolean(
-                formik.errors.confirmPassword
-              )
-            }
-            helperText={
-              formik.touched.confirmPassword &&
-              formik.errors.confirmPassword
-            }
-          />
-          <Button
-            color="secondary"
-            type="submit"
-            variant="contained"
-          >
-            Skapa konto
-          </Button>
+        <TextField
+          id="username"
+          name="username"
+          label="Användarnamn"
+          type="text"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.username && Boolean(formik.errors.username)
+          }
+          helperText={
+            formik.touched.username && formik.errors.username
+          }
+        />
+        <TextField
+          id="password"
+          name="password"
+          label="Lösenord"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.password && Boolean(formik.errors.password)
+          }
+          helperText={
+            formik.touched.password && formik.errors.password
+          }
+        />
+        <TextField
+          id="confirmPassword"
+          name="confirmPassword"
+          label="Bekräfta lösenord"
+          type="password"
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.confirmPassword &&
+            Boolean(formik.errors.confirmPassword)
+          }
+          helperText={
+            formik.touched.confirmPassword &&
+            formik.errors.confirmPassword
+          }
+        />
+        <Button color="secondary" type="submit" variant="contained">
+          Skapa konto
+        </Button>
       </Box>
     </Paper>
   );
