e3136235e9ee49be17580e772ba72ad92465e3bc
edvindjulic <djulicedvin@gmail.com>
2023-04-25 13:01:07 +0200

    "Handledning".

--- client/src/Context/UserContext.tsx
+++ client/src/Context/UserContext.tsx
@@ -1,4 +1,8 @@
-import { createContext, useState, ReactNode, useContext } from "react";
+import {
+  createContext,
+  useEffect,
+  useState,
+} from "react";
 
 interface User {
   _id: string;
@@ -7,25 +11,45 @@ interface User {
 }
 
 interface UserContextType {
-  user: User | null;
-  setUser: (user: User | null) => void;
+  user?: User;
+  setUser: (user?: User) => void;
 }
 
-export const UserContext = createContext<UserContextType>({
-  user: null,
-  setUser: () => {},
-});
+export const UserContext =
+  createContext<UserContextType>({
+    setUser: () => {},
+  });
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function UserProvider({ children }: Props) {
-  const [user, setUser] = useState<User | null>(null);
+export default function UserProvider({
+  children,
+}: Props) {
+  const [user, setUser] = useState<User>();
+// Handledning från David. Hämta user från server vid refresh av sidan.
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "/api/users/me",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setUser(await response.json());
+    };
+    fetchData();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ user, setUser }}
+    >
       {children}
     </UserContext.Provider>
   );
-};
+}
