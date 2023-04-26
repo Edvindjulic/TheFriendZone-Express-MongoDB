d0a31f8669a8c87414245f8f8833d9486b7660fa
Jesper Lindström <108944899+Jesper-Lindstrom@users.noreply.github.com>
2023-04-26 11:43:25 +0200

    add delete button and pair with api/delete

--- client/src/Context/PostContext.tsx
+++ client/src/Context/PostContext.tsx
@@ -1,8 +1,7 @@
-
 import { createContext, useEffect, useState } from "react";
 
-
 interface Post {
+  id: string;
   title: string;
   content: string;
 }
@@ -11,6 +10,7 @@ export const PostContext = createContext(
   {} as {
     posts: Post[];
     addPost: (post: Post) => void;
+    deletePost: (id: string, index: number) => void;
   }
 );
 
@@ -27,33 +27,50 @@ export const PostProvider = ({ children }: { children: React.ReactNode }) => {
     fetchData();
   }, []);
 
- async function addPost(newPost: Post) {
-  try {
-    const response = await fetch("/api/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newPost),
-    });
+  async function addPost(newPost: Post) {
+    try {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPost),
+      });
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Registration successful, user:", data);
-    } else {
-      const message = await response.text();
-      throw new Error(message);
-    } 
-  }catch (error) {
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Registration successful, user:", data);
+      } else {
+        const message = await response.text();
+        throw new Error(message);
+      }
+    } catch (error) {
       console.error("Error registering user:", error);
     }
     setPosts([newPost, ...posts]);
+  }
 
-  };
+  async function deletePost(id: string, index: Number) {
+    try {
+      const response = await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        console.log("post deleted succesfully");
+        const updatedPosts = posts.filter((post, i) => i !== index);
+        setPosts(updatedPosts);
+      } else {
+        console.log("error deleting");
+        const message = await response.text();
+        throw new Error(message);
+      }
+    } catch (error) {
+      console.error("error deleting post", error);
+    }
+  }
 
   return (
-    <PostContext.Provider value={{ posts, addPost }}>
-      {children}
-    </PostContext.Provider>
+    <PostContext.Provider value={{ posts, addPost, deletePost }}>{children}</PostContext.Provider>
   );
 };
