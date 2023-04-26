cb3d2f7b858bbb0fa7b03be9b8afdd3f17641e6d
lysmac <calle@lysmask.se>
2023-04-26 16:15:30 +0200

    now renders all users in a table with admin status, and buttons for remove/change status. Buttons not yet functional

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,4 +1,9 @@
-import { Typography } from "@mui/material";
+import { Button, TableCell, TableHead, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
 import React from "react";
 import { UserContext } from "../Context/UserContext";
 import AccountMenu from "../components/AccountMenu";
@@ -9,7 +14,6 @@ export default function Admin() {
 
   const handleGetAllUsers = async () => {
     const data = await getAllUsers();
-    console.log(data); // Add this line to check the data format
     setAllUsers(data);
   };
   React.useEffect(() => {
@@ -22,12 +26,49 @@ export default function Admin() {
         <>
           <AccountMenu />
           <Typography variant="h3">Admin</Typography>
-          {/* Render the list of users */}
-          <ul>
+          {/* <ul>
             {allUsers.map((user) => (
-              <li key={user._id}>{user.username}</li> // Make sure 'id' and 'name' properties exist in the User interface
+              <li key={user._id}>{user.username}</li>
             ))}
-          </ul>
+          </ul> */}
+
+          <TableContainer component={Paper} sx={{ maxWidth: 700 }}>
+            <Table sx={{ width: "100%" }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Användarnamn</TableCell>
+                  <TableCell align="right">Administratörsstatus</TableCell>
+                  <TableCell align="right">
+                    Ändra administratörsstatus
+                  </TableCell>
+                  <TableCell align="right">Ta bort användare</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {allUsers.map((user) => (
+                  <TableRow
+                    key={user._id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {user.username}
+                    </TableCell>
+                    <TableCell align="right">
+                      {user.isAdmin.toString()}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button type="contained"> Byt status </Button>{" "}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button type="contained" color="error">
+                        Ta bort{" "}
+                      </Button>{" "}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </>
       ) : (
         <Typography variant="h3">Du är inte administratör</Typography>
