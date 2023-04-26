1cfc812aa85c81272889efc4bfb1cb9c4f5a61c8
lysmac <calle@lysmask.se>
2023-04-26 11:34:16 +0200

    login now stays during refresh!

--- client/src/Context/UserContext.tsx
+++ client/src/Context/UserContext.tsx
@@ -1,8 +1,4 @@
-import {
-  createContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, useEffect, useState } from "react";
 
 interface User {
   _id: string;
@@ -15,40 +11,39 @@ interface UserContextType {
   setUser: (user?: User) => void;
 }
 
-export const UserContext =
-  createContext<UserContextType>({
-    setUser: () => {},
-  });
+export const UserContext = createContext<UserContextType>({
+  setUser: () => {},
+});
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function UserProvider({
-  children,
-}: Props) {
+export default function UserProvider({ children }: Props) {
   const [user, setUser] = useState<User>();
-// Handledning från David. Hämta user från server vid refresh av sidan.
+  // Handledning från David. Hämta user från server vid refresh av sidan.
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        "/api/users/me",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      setUser(await response.json());
+      const response = await fetch("/api/users/me", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const userData = await response.json();
+        setUser(userData);
+        // console.log(userData);
+      } else {
+        console.error("Error fetching user data:", response.statusText);
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <UserContext.Provider
-      value={{ user, setUser }}
-    >
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
