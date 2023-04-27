b35fd34dbcd9bd33af5ab94a06e867b3c9b9874f
ye-mou <git.ymo22@gmail.com>
2023-04-27 19:40:33 +0200

    added breakpoints and removed text on small screens

--- client/src/pages/Startpage.tsx
+++ client/src/pages/Startpage.tsx
@@ -1,13 +1,18 @@
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import { Box } from "@mui/system";
-import LoggedIn from "../components/LoggedIn";
-import SignIn from "../components/SignIn";
-import { Typography } from "@mui/material";
 import { useContext } from "react";
-import { UserContext } from "../Context/UserContext";
+import LoggedIn from "../components/LoggedIn";
 import Posts from "../components/Posts";
+import SignIn from "../components/SignIn";
+import { UserContext } from "../Context/UserContext";
 
 export default function Startpage() {
   const { user } = useContext(UserContext);
+  const theme = useTheme();
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
 
   return (
     <Box
@@ -15,7 +20,7 @@ export default function Startpage() {
         height: "auto",
         width: "100%",
         display: "flex",
-        flexDirection: "row",
+        flexDirection: isSmallScreen ? "column" : "row",
       }}
     >
       {user ? (
@@ -24,24 +29,36 @@ export default function Startpage() {
         </Box>
       ) : (
         <>
-          <Box sx={{ width: "30%", margin: "2rem 1rem 4rem 2rem" }}>
-            <Typography
-              variant="h5"
-              sx={{
-                margin: "1rem 0",
-                "& .highlight": {
-                  color: "primary.main",
-                  fontWeight: "bold",
-                },
-              }}
-            >
-              Livet är bättre med vänner – och här på{" "}
-              <span className="highlight">The Friend Zone</span> finns det gott
-              om dem att hitta!
-            </Typography>
+          <Box
+            sx={{
+              width: isSmallScreen ? "100%" : isMediumScreen ? "30%" : "30%",
+              margin: "2rem 1rem 4rem 2rem",
+            }}
+          >
+            {isLargeScreen && (
+              <Typography
+                variant="h5"
+                sx={{
+                  margin: "1rem 0",
+                  "& .highlight": {
+                    color: "primary.main",
+                    fontWeight: "bold",
+                  },
+                }}
+              >
+                Livet är bättre med vänner – och här på{" "}
+                <span className="highlight">The Friend Zone</span> finns det
+                gott om dem att hitta!
+              </Typography>
+            )}
             <SignIn />
           </Box>
-          <Box sx={{ width: "70%", margin: "0rem 3rem" }}>
+          <Box
+            sx={{
+              width: isSmallScreen ? "100%" : isMediumScreen ? "70%" : "70%",
+              margin: "0rem 3rem",
+            }}
+          >
             <Posts />
           </Box>
         </>
