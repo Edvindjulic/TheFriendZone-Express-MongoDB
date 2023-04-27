03b1a2eaf3f9b6e03f7609d3a73619c9cecc2a5e
lysmac <calle@lysmask.se>
2023-04-27 14:52:45 +0200

    now shows username on the single post as well

--- client/src/components/SinglePost.tsx
+++ client/src/components/SinglePost.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { PostContext } from "../Context/PostContext";
 import { UserContext } from "../Context/UserContext";
@@ -7,15 +7,28 @@ import UpdateForm from "./UpdateForm";
 export default function SinglePost() {
   const params = useParams();
   const { posts } = useContext(PostContext);
-  const { user } = useContext(UserContext);
+  const { user, getUsernameById } = useContext(UserContext);
 
   const selectedPost = posts.find((post) => post._id === params.id);
+
+  const [authorName, setAuthorName] = useState("");
+
+  useEffect(() => {
+    const fetchAuthorName = async () => {
+      if (selectedPost) {
+        const username = await getUsernameById(selectedPost.author);
+        setAuthorName(username);
+      }
+    };
+
+    fetchAuthorName();
+  }, [selectedPost, getUsernameById]);
 
   return (
     <>
       Title: {selectedPost?.title} <br />
       Content: {selectedPost?.content} <br />
-      Author: {selectedPost?.author} <br />
+      Author: {authorName} <br />
       {user && (selectedPost?.author === user._id || user.isAdmin) ? (
         <UpdateForm id={selectedPost?._id} />
       ) : (
